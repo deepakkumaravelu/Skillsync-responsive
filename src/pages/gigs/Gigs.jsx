c34eec05a02bb7af16bda5d8243a0bce8473fcd9
Deepak Kumaravelu <deepak1122003kumar@gmail.com>
2024-02-25 21:11:58 +0530

    updated gigs and gigcard

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/no-unescaped-entities */
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Gigs.scss";
 import GigCard from "../../components/gigCard/GigCard";
 import newRequest from "../../utils/newRequest";
-import { useQuery} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { useLocation } from "react-router-dom";
 function Gigs() {
   const [sort, setSort] = useState("sales");
@@ -11,31 +11,43 @@ function Gigs() {
   const minRef = useRef();
   const maxRef = useRef();
 
-  const {search}=useLocation();
-  const { isLoading, error, data ,refetch } = useQuery({
-    queryKey: ['repoData'],
+  const { search } = useLocation();
+  const { isLoading, error, data, refetch } = useQuery({
+    queryKey: ["gigs"],
     queryFn: () =>
-      newRequest.get(`/gigs${search}&min=${minRef.current.value}&max=${maxRef.current.value}`).then(res=>{
-        return res.data;
-      }),
-  })
+      newRequest
+        .get(
+          `/gigs${search}&min=${minRef.current.value}&max=${maxRef.current.value}&sort=${sort}`
+        )
+        .then((res) => {
+          return res.data;
+        }),
+  });
   console.log(data);
   const reSort = (type) => {
     setSort(type);
     setOpen(false);
   };
-
-  const apply = ()=>{
+  useEffect(
+    () => {
+      refetch();
+    },
+    [sort] 
+  );
+  const apply = () => {
     refetch();
-  }
+  };
 
   return (
     <div className="gigs">
       <div className="container">
-        <span className="breadcrumbs">SkillSync {">"} Graphics & Design {">"}</span>
+        <span className="breadcrumbs">
+          SkillSync {">"} Graphics & Design {">"}
+        </span>
         <h1>AI Artists</h1>
         <p>
-          Explore the boundaries of art and technology with SkillSync's AI artists
+          Explore the boundaries of art and technology with SkillSync's AI
+          artists
         </p>
         <div className="menu">
           <div className="left">
@@ -56,17 +68,18 @@ function Gigs() {
                   <span onClick={() => reSort("createdAt")}>Newest</span>
                 ) : (
                   <span onClick={() => reSort("sales")}>Best Selling</span>
-                  )}
-                  <span onClick={() => reSort("sales")}>Popular</span>
+                )}
+                <span onClick={() => reSort("sales")}>Popular</span>
               </div>
             )}
           </div>
         </div>
         <div className="cards">
-          {isLoading ? "loading": error ? "Something went wrong"
-            :data.map((gig) => (
-            <GigCard key={gig._id} item={gig} />
-          ))}
+          {isLoading
+            ? "loading"
+            : error
+            ? "Something went wrong"
+            : data.map((gig) => <GigCard key={gig._id} item={gig} />)}
         </div>
       </div>
     </div>
