35ed12d885a0d3ec5e029d9c0c69da0cb3687542
Deepak Kumaravelu <deepak1122003kumar@gmail.com>
2024-03-31 21:26:17 +0530

    review system updated

--- src/pages/gig/Gig.jsx
+++ src/pages/gig/Gig.jsx
@@ -5,6 +5,7 @@ import { Carousel } from 'antd';
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
+import Reviews from "../../components/reviews/Reviews";
 
 function Gig() {
   const onChange = function(currentSlide) {
@@ -72,7 +73,7 @@ function Gig() {
             </div>
           )}
           <Carousel afterChange={onChange} className="slider" autoplay={true} autoplaySpeed={2000} speed={1000}>
-                <div>
+                {/* <div>
                 <img key={data.images[0]} src={data.images[0]} alt="" />
                 </div>
                 <div>
@@ -80,49 +81,43 @@ function Gig() {
                 </div>
                 <div>
                 <img key={data.images[2]} src={data.images[2]} alt="" />
-                </div>
+                </div> */}
+                {data.images.map((img)=>(
+                  <img key={img} src={img} alt=""/>
+                ))}
         </Carousel>
           <h2>About This Gig</h2>
           <p>
-            I use an AI program to create images based on text prompts. This
-            means I can help you to create a vision you have through a textual
-            description of your scene without requiring any reference images.
-            Some things I've found it often excels at are: Character portraits
-            (E.g. a picture to go with your DnD character) Landscapes (E.g.
-            wallpapers, illustrations to compliment a story) Logos (E.g. Esports
-            team, business, profile picture) You can be as vague or as
-            descriptive as you want. Being more vague will allow the AI to be
-            more creative which can sometimes result in some amazing images. You
-            can also be incredibly precise if you have a clear image of what you
-            want in mind. All of the images I create are original and will be
-            found nowhere else. If you have any questions you're more than
-            welcome to send me a message.
+           {data.desc}
           </p>
           <div className="seller">
             <h2>About The Seller</h2>
+            {isLoadingUser ? ("Loading.."):errorUser ?("Something went wrong!"):(
             <div className="user">
               <img
-                src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                src={dataUser.img|| "/img/noavatar.jpg" }
                 alt=""
               />
               <div className="info">
-                <span>Anna Bell</span>
+                <span>{dataUser.username}</span>
+                {!isNaN(data.totalStars/data.starNumber)&&(
                 <div className="stars">
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <img src="/img/star.png" alt="" />
-                  <span>5</span>
-                </div>
+                   {Array(Math.round(data.totalStars / data.starNumber))
+                    .fill()
+                    .map((item, i) => (
+                      <img src="/img/star.png" alt="" key={i} />
+                    ))}
+                  <span>{Math.round(data.totalStars / data.starNumber)}</span>
+                </div>)}
                 <button>Contact Me</button>
               </div>
-            </div>
+            </div>)}
+            {isLoadingUser ? ("Loading.."):errorUser ?("Something went wrong!"):(
             <div className="box">
               <div className="items">
                 <div className="item">
                   <span className="title">From</span>
-                  <span className="desc">USA</span>
+                  <span className="desc">{dataUser.country}</span>
                 </div>
                 <div className="item">
                   <span className="title">Member since</span>
@@ -143,169 +138,38 @@ function Gig() {
               </div>
               <hr />
               <p>
-                My name is Anna, I enjoy creating AI generated art in my spare
-                time. I have a lot of experience using the AI program and that
-                means I know what to prompt the AI with to get a great and
-                incredibly detailed result.
+               {dataUser.desc}
               </p>
-            </div>
+            </div>)}
           </div>
-          <div className="reviews">
-            <h2>Reviews</h2>
-            <div className="item">
-              <div className="user">
-                <img
-                  className="pp"
-                  src="https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <div className="info">
-                  <span>Garner David</span>
-                  <div className="country">
-                    <img
-                      src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
-                      alt=""
-                    />
-                    <span>United States</span>
-                  </div>
-                </div>
-              </div>
-              <div className="stars">
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <span>5</span>
-              </div>
-              <p>
-                I just want to say that art_with_ai was the first, and after
-                this, the only artist Ill be using on Fiverr. Communication was
-                amazing, each and every day he sent me images that I was free to
-                request changes to. They listened, understood, and delivered
-                above and beyond my expectations. I absolutely recommend this
-                gig, and know already that Ill be using it again very very soon
-              </p>
-              <div className="helpful">
-                <span>Helpful?</span>
-                <img src="/img/like.png" alt="" />
-                <span>Yes</span>
-                <img src="/img/dislike.png" alt="" />
-                <span>No</span>
-              </div>
-            </div>
-            <hr />
-            <div className="item">
-              <div className="user">
-                <img
-                  className="pp"
-                  src="https://images.pexels.com/photos/4124367/pexels-photo-4124367.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <div className="info">
-                  <span>Sidney Owen</span>
-                  <div className="country">
-                    <img
-                      src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1e9-1f1ea.png"
-                      alt=""
-                    />
-                    <span>Germany</span>
-                  </div>
-                </div>
-              </div>
-              <div className="stars">
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <span>5</span>
-              </div>
-             
-
-              <p>
-                The designer took my photo for my book cover to the next level!
-                Professionalism and ease of working with designer along with
-                punctuality is above industry standards!! Whatever your project
-                is, you need this designer!
-              </p>
-              <div className="helpful">
-                <span>Helpful?</span>
-                <img src="/img/like.png" alt="" />
-                <span>Yes</span>
-                <img src="/img/dislike.png" alt="" />
-                <span>No</span>
-              </div>
-            </div>
-            <hr />
-            <div className="item">
-              <div className="user">
-                <img
-                  className="pp"
-                  src="https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <div className="info">
-                  <span>Lyle Giles </span>
-                  <div className="country">
-                    <img
-                      src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
-                      alt=""
-                    />
-                    <span>United States</span>
-                  </div>
-                </div>
-              </div>
-              <div className="stars">
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <span>5</span>
-              </div>
-              <p>
-                Amazing work! Communication was
-                amazing, each and every day he sent me images that I was free to
-                request changes to. They listened, understood, and delivered
-                above and beyond my expectations. I absolutely recommend this
-                gig, and know already that Ill be using it again very very soon
-              </p>
-              <div className="helpful">
-                <span>Helpful?</span>
-                <img src="/img/like.png" alt="" />
-                <span>Yes</span>
-                <img src="/img/dislike.png" alt="" />
-                <span>No</span>
-              </div>
-            </div>
-          </div>
+          <Reviews gigId={id}/>
         </div>
         <div className="right">
           <div className="price">
-            <h3>1 AI generated image</h3>
-            <h2>₹ 59.99</h2>
+            <h3>{data.shortTitle}</h3>
+            <h2>₹ {data.price}</h2>
           </div>
           <p>
-            I will create a unique high quality AI generated image based on a
-            description that you give me
+           {data.shortDesc}
           </p>
           <div className="details">
             <div className="item">
               <img src="/img/clock.png" alt="" />
-              <span>2 Days Delivery</span>
+              <span>{data.deliveryTime} Days</span>
             </div>
             <div className="item">
               <img src="/img/recycle.png" alt="" />
-              <span>3 Revisions</span>
+              <span>{data.revisionNumber} Revisions</span>
             </div>
           </div>
           <div className="features">
-            <div className="item">
-              <img src="/img/violetcheck.png" alt="" />
-              <span>Prompt writing</span>
+            {data.features.map((feature)=>(
+            <div className="item"key={feature}>
+            <img src="/img/violetcheck.png" alt="" />
+            <span>{feature}</span>
             </div>
-            <div className="item">
+            ))}
+            {/* <div className="item">
               <img src="/img/violetcheck.png" alt="" />
               <span>Artwork delivery</span>
             </div>
@@ -316,7 +180,7 @@ function Gig() {
             <div className="item">
               <img src="/img/violetcheck.png" alt="" />
               <span>Additional design</span>
-            </div>
+            </div> */}
           </div>
           <button>Continue</button>
         </div>
